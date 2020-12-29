7168d1b1e61352a5c1fa52a2ec1b02f9626a3679
Sophia Jung <sophiabraddock82@gmail.com>
2020-12-28 22:37:06 -0500

    posts, firebase db

--- src/Feed.js
+++ src/Feed.js
@@ -1,30 +1,85 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Feed.css";
 import CreateIcon from "@material-ui/icons/Create";
 import InputOption from "./InputOption";
 import ImageIcon from "@material-ui/icons/Image";
-import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
-import EventNoteIcon from '@material-ui/icons/EventNote';
-import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
+import Post from "./Post";
+import { db } from "./firebase";
+import firebase from "firebase";
 
 function Feed() {
+  const [input, setInput] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  //   creates a realtime listener to firebase - when a post is pushed to the database  - this is directly mapped to the posts - when the posts changed its updated
+  useEffect(() => {
+    db.collection("posts")
+    .orderBy('timestamp', 'desc')
+    .onSnapshot((snapshot) =>
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      )
+    );
+  }, []);
+
+  const sendPost = (e) => {
+    e.preventDefault();
+
+    db.collection("posts").add({
+      name: "Sara Thomasin",
+      description: "this is a test",
+      message: input,
+      photoUrl: "",
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+    setInput("");
+  };
+
   return (
     <div className="feed">
       <div className="feed__inputContainer">
         <div className="feed__input">
           <CreateIcon />
           <form action="">
-            <input type="text" />
-            <button type="submit">Send</button>
+            <input
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              type="text"
+            />
+            <button onClick={sendPost} type="submit">
+              Send
+            </button>
           </form>
         </div>
+
         <div className="feed__inputOptions">
           <InputOption Icon={ImageIcon} title="Photo" color="#70b5f9" />
           <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
           <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
-          <InputOption Icon={CalendarViewDayIcon} title="Write article" color="#7FC15E" />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="Write article"
+            color="#7FC15E"
+          />
         </div>
       </div>
+      {/* posts */}
+      {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
+        <Post
+          key={id}
+          name={name}
+          description={description}
+          message={message}
+          photoUrl={photoUrl}
+        />
+      ))}
     </div>
   );
 }
